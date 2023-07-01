4b53fe3e40433eb051907abfd61fe7144bcc87e6
ITboo <annaacdc@tut.by>
2023-07-01 20:22:50 +0300

    feat: add table-tests solution

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,50 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  //ADD
+  { a: 1, b: 3, action: Action.Add, expected: 4 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 1, action: Action.Add, expected: 4 },
+  //SUBSTRACT
+  { a: 1, b: 3, action: Action.Substract, expected: -2 },
+  { a: 2, b: 2, action: Action.Substract, expected: 0 },
+  { a: 3, b: 1, action: Action.Substract, expected: 2 },
+  //MULTIPLY
+  { a: 1, b: 3, action: Action.Multiply, expected: 3 },
+  { a: 2, b: 2, action: Action.Multiply, expected: 4 },
+  { a: 3, b: 1, action: Action.Multiply, expected: 3 },
+  //DIVIDE
+  { a: 9, b: 3, action: Action.Divide, expected: 3 },
+  { a: 8, b: 2, action: Action.Divide, expected: 4 },
+  { a: 7, b: 1, action: Action.Divide, expected: 7 },
+  //ESPONENTIATE
+  { a: 1, b: 1, action: Action.Exponentiate, expected: 1 },
+  { a: 2, b: 2, action: Action.Exponentiate, expected: 4 },
+  { a: 3, b: 3, action: Action.Exponentiate, expected: 27 },
+  //RETURN NULL (ACTION)
+  { a: 1, b: 3, action: 'action', expected: null },
+  { a: 2, b: 2, action: undefined, expected: null },
+  { a: 3, b: 1, action: null, expected: null },
+  //RETURN NULL (ARG)
+  { a: 1, b: '3', action: Action.Exponentiate, expected: null },
+  { a: 2, b: null, action: Action.Exponentiate, expected: null },
+  { a: 3, b: undefined, action: Action.Exponentiate, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test('should return expected', () => {
+    testCases.forEach((test) => {
+      const { a, b, action, expected } = test;
+      const result = simpleCalculator({
+        a: a,
+        b: b,
+        action: action,
+      });
+      if (expected === null) {
+        expect(result).toBeNull();
+      } else {
+        expect(result).toEqual(expected);
+      }
+    });
   });
-  // Consider to use Jest table tests API to test all cases above
 });
